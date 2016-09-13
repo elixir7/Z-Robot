da09d7d575318ebda74da78bdf07fa819baaa2d5
Bjarnen <bjarnen.123456789@gmail.com>
2016-09-13 11:35:50 +0200

    Added version 2 which has the correct path

---
+++ Z/follow-the-Z-v2/follow-the-Z-v2.ino
@@ -0,0 +1,146 @@
+#include <Servo.h>
+
+Servo servoRight;
+Servo servoLeft;
+
+void setup() {
+  servoLeft.attach(11);
+  servoRight.attach(10);
+
+  pinMode(7, INPUT); //Right whisker
+  pinMode(5, INPUT); //Left whisker
+  pinMode(9, OUTPUT); //Speaker
+  pinMode(8, OUTPUT); //Left LED
+  pinMode(2, OUTPUT); //Right LED
+  Serial.begin(9600);
+
+  tone(9, 3000, 1000);
+  delay(2000);
+
+  //playMusic();
+
+  forward(4);
+  //maneuver(1520, 1390, 1000);
+  //maneuver(1700, 1470, 4200);
+  
+  turn("right", 50);
+  //forward(5);
+  forward(35);
+  //maneuver(1650, 1480, 1000);
+  //maneuver(1520, 1390, 4200);
+
+  turn("left", 100);
+  
+  forward(15);
+
+  maneuver(1300, 1300, 1000);
+  playMusic();
+
+  forward(4);
+  turn("right", 50);
+  
+  forward(35);
+  //maneuver(1650, 1480, 1000);
+  //maneuver(1520, 1390, 4200);
+
+  turn("left", 100);
+  
+  forward(15);
+}
+
+void loop() {
+  byte wRight = digitalRead(7);
+  byte wLeft = digitalRead(5);
+
+  //Turn led on depending on whiskers
+  if(wRight == 0){
+    digitalWrite(2, HIGH);
+  }else if(wLeft == 0){
+    digitalWrite(8, HIGH);
+  }else{
+    digitalWrite(2, LOW);
+    digitalWrite(8, LOW);
+  }
+}
+
+void forward(double distance){
+  double speed = 0.16;
+  double time = (distance/100) / speed;
+  servoLeft.writeMicroseconds(1700);
+  servoRight.writeMicroseconds(1390);
+  delay(time*1000);
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1500); 
+}
+
+void turn(String direction, int turn){
+  int turnR = 0;
+  if(direction == "right"){
+    for(int moment = 0; moment <= turn; moment += 1){
+      if (turnR != 100){
+        turnR += 3;
+      }
+      //Serial.print(speed);
+      servoLeft.writeMicroseconds(1700);
+      servoRight.writeMicroseconds(1390 + turnR);
+      delay(40);
+    }
+  }
+  else if(direction == "left"){
+    for(int moment = 0; moment <= turn; moment += 1){
+      Serial.print(turnR);
+      if (turnR != 200){
+        turnR += 5;
+      }
+      servoLeft.writeMicroseconds(1700 - turnR);
+      servoRight.writeMicroseconds(1390);
+      delay(20);
+    }
+  }
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1500);
+  turnR = 0;
+}
+
+void maneuver(int speedLeft, int speedRight, int msTime){
+  servoLeft.writeMicroseconds(speedLeft);
+  servoRight.writeMicroseconds(speedRight);
+  if(msTime == -1){
+    servoLeft.detach();
+    servoRight.detach();
+  }
+  delay(msTime);
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1500);
+}
+
+void playMusic(){
+   tone(9, 1568, 500);
+  delay(500);
+  tone(9, 1568, 500);
+  delay(500);
+  tone(9, 1568, 500);
+  delay(500);
+  tone(9, 1568, 500);
+  delay(500);
+  
+  tone(9, 1865, 300);
+  delay(300);
+  tone(9, 2093, 300);
+  delay(300);
+
+  tone(9, 1568, 500);
+  delay(500);
+  tone(9, 1568, 500);
+  delay(500);
+  tone(9, 1568, 500);
+  delay(500);
+  tone(9, 1568, 500);
+  delay(500);
+
+  tone(9, 1397, 400);
+  delay(400);
+  tone(9, 1480, 300);
+  delay(300);
+}
+
