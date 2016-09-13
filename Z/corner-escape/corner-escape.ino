9df436de3b0fc22c41bc1a2d85637458255c9796
Isak <aslundisak@gmail.com>
2016-09-13 12:32:11 +0200

    Added assignment for making the bot escape a corner

---
+++ Z/corner-escape/corner-escape.ino
@@ -0,0 +1,10 @@
+//Program that let's the bot escape a corner with the use of whiskers
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
